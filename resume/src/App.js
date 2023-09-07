4ac43ca4519eced5279186cd40036b98f16656de
Fabian Lien <fabian.lien@gmail.com>
2023-09-07 22:55:47 +0200

    add redirect to /home

--- resume/src/App.js
+++ resume/src/App.js
@@ -4,7 +4,7 @@ import Contact from "./pages/Contact";
 import Github from "./pages/Github";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Container from "react-bootstrap/Container";
 
@@ -13,10 +13,11 @@ function App() {
     <Container fluid="xl" className="p-0">
       <Nav />
       <Routes>
+        <Route exact path="/" element={<Navigate to="/home" />} />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/resume" element={<Resume />} />
-        <Route exact path= "/contact" element={<Contact />} />
-        <Route exact path= "/github" element={<Github />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/github" element={<Github />} />
       </Routes>
       <Footer />
     </Container>
